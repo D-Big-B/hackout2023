906be142e7318b76ddc16d5ce485ae0069e78457
D-Big-B <prjjwlbhatt@gmail.com>
2023-11-04 22:56:32 +0530

    VR functionality added

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
+
 const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-const { ExpressPeerServer } = require("peer");
+import httpserver from "http";
+const server = httpserver.Server(app);
+// const server = require("http").Server(app);
+// const io = require("socket.io")(server);
+// import ioModule from "socket.io";
+// const io = ioModule(server);
+import { Server } from "socket.io";
+
+const io = new Server(server);
+import { ExpressPeerServer } from "peer";
+// const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
-const { v4: uuidV4 } = require("uuid");
-
+import { VRButton } from "three/addons/webxr/VRButton.js";
+// const {
+//   VRButton,
+// } = require("./node_modules/three/examples/jsm/webxr/VRButton.js");
+// const { v4: uuidV4 } = require("uuid");
+import { v4 as uuidV4 } from "uuid";
 app.use("/peerjs", peerServer);
 
 app.set("view engine", "ejs");
@@ -18,7 +32,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
+  res.render("room", { roomId: req.params.room, VRButton });
 });
 
 io.on("connection", (socket) => {
